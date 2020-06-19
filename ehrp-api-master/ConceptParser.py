866a3d85e0e18827d7eb894b38afac4dbb26a9e6
Keith Zmudzinski <kzmudz79@gmail.com>
2020-06-19 15:06:29 -0700

    added ami graph and tables

--- ehrp-api-master/ConceptParser.py
+++ ehrp-api-master/ConceptParser.py
@@ -33,8 +33,8 @@ class ConceptParser:
             'drugParser': self.drugParser,
             'disorderParser': self.disorderParser,
             'prescriptionParser': self.prescriptionParser,
-            'diseaseParser': self.diseaseParser,
             'chfParser': self.chfParser,
+            'amiParser': self.amiParser,
         }
 
     def setup(self):
@@ -286,14 +286,24 @@ class ConceptParser:
 
         return concepts
 
-    def diseaseParser(self, contexts, id_dict, onto_dict):
-        concepts = self.make_concepts_object('disease')
-        for i, context in enumerate(contexts):
-            print('Entry {}'.format(i), context)
-        return concepts
-
     def chfParser(self, contexts, id_dict, onto_dict):
         concepts = self.make_concepts_object('chf')
+
+        for i, context in enumerate(contexts):
+            parts = context.split('\t')
+            trigger = parts[1]
+            trigger, key = trigger.split('__')
+            context = parts[0] + trigger + parts[2]
+
+            concepts['instances'].append({
+                'type': key,
+                'trigger': trigger,
+                'context': context
+            })
+        return concepts
+
+    def amiParser(self, contexts, id_dict, onto_dict):
+        concepts = self.make_concepts_object('ami')
 
         for i, context in enumerate(contexts):
             parts = context.split('\t')

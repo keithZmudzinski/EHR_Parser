67ccb74c8b7899726be99c4732e7762aaaeae1d2
Ulyana Tkachenko <utkac001@ucr.edu>
2020-08-05 18:38:35 -0700

    Update to modified misspellings using a space

--- misspelling.py
+++ misspelling.py
@@ -10,12 +10,25 @@ import re
 import pandas as pd
 import numpy as np
 from string_grouper import match_most_similar
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix
+import sparse_dot_topn.sparse_dot_topn as ct
 
 base_url = 'http://localhost:8020/ehrp/'
 extract_url = 'extract'
 lookup_url = 'lookup'
 file = 'pitt.txt'
 group_types = ""
+text = "test"
+
+def remove_file(filePath):
+    if os.path.exists(filePath):
+        print("Removing: \"", filePath, "\"")
+        os.remove(filePath)
+        return 1
+    else:
+        print("File: \"", filePath, "\" was not removed because it doesn't exist")
+    return 0
 
 def preprocess_json(json):
     resp_json = json
@@ -57,8 +70,8 @@ def build_potential_corpus(corpus, sorted_labels):
 
 def get_label_length_df(sorted_labels):
     labels_df =  pd.DataFrame(sorted_labels, columns =['label'])
-    labels_df['len'] = labels_df['label'].apply(lambda x: len(x.split()))
-    return labels_df
+    labels_df['len'] = labels_df['label'].apply(lambda x: len(re.split(r'[\s\-]', x)))
+    return (labels_df, labels_df['len'].max())
 
 def split_n_grams_s(text, N):
     text = text.split()
@@ -70,28 +83,110 @@ def split_n_grams_s(text, N):
 
     grams_s =  pd.Series(gram_list)
     return grams_s
-
+## cosine_faster
 def ngrams(string, n=3):
     string = re.sub(r'[,-./]|\sBD',r'', string)
     ngrams = zip(*[string[i:] for i in range(n)])
     return [''.join(ngram) for ngram in ngrams]
+
+def cosine_faster_csr(A, B, ntop, lower_bound=0):
+    f_time = time.time()
+    # force A and B as a CSR matrix.
+    # If they have already been CSR, there is no overhead
+    A = A.tocsr()
+    B = B.tocsr()
+    M, _ = A.shape
+    _, N = B.shape
+ 
+    idx_dtype = np.int32
+ 
+    nnz_max = M*ntop
+ 
+    indptr = np.zeros(M+1, dtype=idx_dtype)
+    indices = np.zeros(nnz_max, dtype=idx_dtype)
+    data = np.zeros(nnz_max, dtype=A.dtype)
+
+    ct.sparse_dot_topn(
+        M, N, np.asarray(A.indptr, dtype=idx_dtype),
+        np.asarray(A.indices, dtype=idx_dtype),
+        A.data,
+        np.asarray(B.indptr, dtype=idx_dtype),
+        np.asarray(B.indices, dtype=idx_dtype),
+        B.data,
+        ntop,
+        lower_bound,
+        indptr, indices, data)
+
+    print('Time taken to return csr matrix:', time.time()-f_time)
+
+    return csr_matrix((data,indices,indptr),shape=(M,N))
+
+def get_matches_df(sparse_matrix, name_vector, top=100):
+    non_zeros = sparse_matrix.nonzero()
+    
+    sparserows = non_zeros[0]
+    sparsecols = non_zeros[1]
+    
+    if top:
+        nr_matches = top
+    else:
+        nr_matches = sparsecols.size
+    
+    left_side = np.empty([nr_matches], dtype=object)
+    right_side = np.empty([nr_matches], dtype=object)
+    similairity = np.zeros(nr_matches)
+    
+    for index in range(0, nr_matches):
+        left_side[index] = name_vector[sparserows[index]]
+        right_side[index] = name_vector[sparsecols[index]]
+        similairity[index] = sparse_matrix.data[index]
+    
+    return pd.DataFrame({'left_side': left_side,
+                          'right_side': right_side,
+                           'similairity': similairity})
+        
+
 
 def catch_misspellings_cosine(corpus, sorted_labels, ratio):
     f_time = time.time()
     corpus_clean = build_potential_corpus(corpus, sorted_labels)
     with open(corpus_clean, 'r') as infile:
         text = infile.read()
-    labels = get_label_length_df(sorted_labels)
-    text =  split_n_grams_s(text, 2)
-
-    # USING sudo pip install string-grouper
+    (labels, max_label_len) = get_label_length_df(sorted_labels)
+    text = split_n_grams_s(text, max_label_len)
     labels = pd.Series(labels['label'])
-    # Create all matches:
-    matches = match_most_similar(text, labels)
-    # Display the results:
+
+    matches = match_most_similar(text, labels, regex='[,./]|\s', min_similarity=ratio)
+
     df = pd.DataFrame({'labels': labels, 'misspellings': matches})
-    df = df.query("labels != misspellings")
-    df.to_csv('test_matches_drug.csv')
+    df = df.query("labels != misspellings") 
+    df.to_csv('text_matches_drug.csv')
+
+   # df_matches = pd.DataFrame(columns=['labels', 'misspelling'])
+    # for i in range(2, max_label_len+2):
+    #     text_ngrams =  split_n_grams_s(text, i)
+    #     print(text_ngrams)
+    #     q = "len == " + str(i - 1)
+    #     print(q)
+    #     label_ngrams = labels.query(q)
+    #     print(label_ngrams.head())
+    #     slabel_ngrams = pd.Series(label_ngrams['label'])
+    #     match_ngrams = match_most_similar(text_ngrams, slabel_ngrams, regex='[,./]|\s', min_similarity=ratio)
+    #     df_ngrams = pd.DataFrame({'labels': slabel_ngrams, 'misspelling': match_ngrams})
+    #     df_ngrams = df_ngrams.query("labels != misspelling")
+    #     f = 'test_matches_drug_' + str(i) + '.csv'
+    #     df_ngrams.to_csv(f)
+    
+    
+    
+    
+    # USING sudo pip install string-grouper
+    # labels = pd.Series(labels['label'])
+    # Create all matches:
+    # matches = match_most_similar(text, labels)
+    # Display the results:
+    # df = pd.DataFrame({'labels': labels, 'misspellings': matches})
+    # df = df.query("labels != misspellings")
     print('Time taken to find misspellings:', time.time()-f_time)
 
 #  pip install python-Levenshtein
@@ -118,6 +213,48 @@ def catch_misspellings_levenshtein(sorted_labels, ratio):
     print("Number of groups in misspellings file: ", n_groups)
     
     return label_groups
+
+def catch_misspellings_cosine_faster(corpus, sorted_labels, ratio):
+    f_time = time.time()
+    corpus_clean = build_potential_corpus(corpus, sorted_labels)
+    with open(corpus_clean, 'r') as infile:
+        text = infile.read()
+    (labels_df, size) = get_label_length_df(sorted_labels)
+    print(labels_df.head())
+    # text =  split_n_grams_s(text, 2)
+    # labels = labels_df['label']
+    vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams)
+    # tf_idf_matrix_label = vectorizer.fit_transform(labels)
+    # tf_idf_matrix_text = vectorizer.fit_transform(text)
+    print(ngrams('today catheter'))
+    
+    labels = labels_df['label']
+    v_time = time.time()
+    tf_idf_matrix_label = vectorizer.fit_transform(labels)
+    print('Time taken to vectorize labels:', time.time()-v_time)
+    
+    text =  split_n_grams_s(text, 2).tolist() #could be made more efficient
+    v_time = time.time()
+    tf_idf_matrix_text = vectorizer.fit_transform(text)
+    print('Time taken to vectorize text-ngrams:', time.time()-v_time)
+    matches = cosine_faster_csr(tf_idf_matrix_label, tf_idf_matrix_text, 10, 0.8)
+
+    matches_df = get_matches_df(matches, labels, top=100000)
+    matches_df = matches_df[matches_df['similairity'] < 0.99999] # Remove all exact matches
+    print(matches_df.sample(20))
+
+
+    # # USING sudo pip install string-grouper
+    # labels = pd.Series(labels['label'])
+    # # Create all matches:
+    # matches = match_most_similar(text, labels)
+    # # Display the results:
+    # df = pd.DataFrame({'labels': labels, 'misspellings': matches})
+    
+    
+    # df = df.query("labels != misspellings")
+    # df.to_csv('test_matches_drug.csv')
+    # print('Time taken to find misspellings:', time.time()-f_time)
 
 def extract(corpus, types=[]):
     args = {}
@@ -168,7 +305,8 @@ def main():
         resp_json = preprocess_json(resp.json())
 
         sorted_labels = create_sorted_labels(resp_json)
-        catch_misspellings_cosine(corpus, sorted_labels, 90)
+        catch_misspellings_cosine(corpus, sorted_labels, 0.8)
+        remove_file('no_key_word_drug.txt')
         #catch_misspellings_levenshtein(sorted_labels, 90)
 
     else:

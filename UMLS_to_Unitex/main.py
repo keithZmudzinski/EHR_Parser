845236f44a716914eb0dbed2bbcd1e9e603c20dc
Keith Zmudzinski <kzmudz79@gmail.com>
2020-08-17 10:33:10 -0700

    Updated dlc and dlf files

--- UMLS_to_Unitex/main.py
+++ UMLS_to_Unitex/main.py
@@ -3,6 +3,7 @@ import sys
 import os
 from convert_to_unitex import umls_to_unitex
 from create_homonyms import create_homonyms
+from create_dlc_dlf import create_dlc_dlf
 
 def main():
     # Point these to your MRCONSO and MRSTY files
@@ -10,13 +11,17 @@ def main():
     types_path = '/home/keith/Documents/Paea Intern/UMLS-2020AA_Full_Install/2020AA/META/MRSTY.RRF'
 
     # Combine MRCONSO and MRSTY to create a unitex-style dictionary
-    umls_to_unitex(conso_path, types_path, 'umls.dic')
+    # umls_to_unitex(conso_path, types_path, 'umls.dic')
 
     # Combine homonyms in dictionary into one entry per homonym
-    create_homonyms('umls.dic', 'Categorized Dictionaries')
+    #   and separate into category files
+    # create_homonyms('umls.dic', 'Categorized Dictionaries')
+
+    # Combine complex words into dlc and simple words into dlf
+    create_dlc_dlf('Categorized Dictionaries')
 
     # Remove intermediate files
-    os.remove('umls.dic')
+    # os.remove('umls.dic')
 
     print('Done!')
 

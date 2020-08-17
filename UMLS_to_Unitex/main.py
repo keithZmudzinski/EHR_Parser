73f708935866beb9ade5405dc526da0e67935156
Keith Zmudzinski <kzmudz79@gmail.com>
2020-08-17 11:49:21 -0700

    Compress dictionaries

--- UMLS_to_Unitex/main.py
+++ UMLS_to_Unitex/main.py
@@ -4,6 +4,7 @@ import os
 from convert_to_unitex import umls_to_unitex
 from create_homonyms import create_homonyms
 from create_dlc_dlf import create_dlc_dlf
+from compress_dictionaries import compress_dictionaries
 
 def main():
     # Point these to your MRCONSO and MRSTY files
@@ -11,17 +12,20 @@ def main():
     types_path = '/home/keith/Documents/Paea Intern/UMLS-2020AA_Full_Install/2020AA/META/MRSTY.RRF'
 
     # Combine MRCONSO and MRSTY to create a unitex-style dictionary
-    # umls_to_unitex(conso_path, types_path, 'umls.dic')
+    umls_to_unitex(conso_path, types_path, 'umls.dic')
 
     # Combine homonyms in dictionary into one entry per homonym
     #   and separate into category files
-    # create_homonyms('umls.dic', 'Categorized Dictionaries')
+    create_homonyms('umls.dic', 'Categorized_Dictionaries')
 
     # Combine complex words into dlc and simple words into dlf
-    create_dlc_dlf('Categorized Dictionaries')
+    create_dlc_dlf('Categorized_Dictionaries')
+
+    # Compress category dictionaries
+    compress_dictionaries('Categorized_Dictionaries')
 
     # Remove intermediate files
-    # os.remove('umls.dic')
+    os.remove('umls.dic')
 
     print('Done!')
 

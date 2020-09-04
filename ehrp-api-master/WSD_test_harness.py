0e9c5c6342149a6347df24697d9bfed8b8ee1b76
Keith Zmudzinski <kzmudz79@gmail.com>
2020-09-03 18:27:35 -0700

    Setting up pseudocode for testing model

---
+++ ehrp-api-master/WSD_test_harness.py
@@ -0,0 +1,8 @@
+from WordSenseDisambiguation import WSD
+
+def get_data():
+    return test, train
+
+def evaluate(model, data):
+    # generate bunha info and scores
+    return
--- ehrp-api-master/WordSenseDisambiguation.py
+++ ehrp-api-master/WordSenseDisambiguation.py
@@ -1,5 +1,15 @@
 ''' Used for the determination of homonym meanings, given context '''
 
-def get_meaning(instances, term, context):
-    # placeholder
-    return instances[0]
+class WSD:
+    def __init__(self):
+        self.model = self.load_model()
+
+    def get_meaning(self, instances, term, context):
+        # placeholder
+        return instances[0]
+
+    def load_model(self):
+        pass
+
+    def save_model(self):
+        pass

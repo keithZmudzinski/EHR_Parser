bf796fc96d64f9018ceed27b00766484e0f87bc1
Keith Zmudzinski <kzmudz79@gmail.com>
2020-09-04 10:59:43 -0700

    Updated layout

--- ehrp-api-master/WordSenseDisambiguation.py
+++ ehrp-api-master/WordSenseDisambiguation.py
@@ -1,7 +1,8 @@
 ''' Used for the determination of homonym meanings, given context '''
+import os
 
 class WSD:
-    def __init__(self, location=None):
+    def __init__(self, location=os.path.join('resources', 'WSD_model.txt')):
         self.location = location
         self.model = self.load_model()
 
@@ -9,10 +10,14 @@ class WSD:
         # placeholder
         return instances[0]
 
-    def load_model(self, path=self.location):
-        if self.location:
-            return loaded_model
-        return None
+    def load_model(self, path=None):
+        model_path = path if path else self.location
 
-    def save_model(self, path=self.location):
+        if model_path == 'NEW_MODEL':
+            return None
+
+        # LOAD MODEL HERE
+        return model
+
+    def save_model(self, path=None):
         pass

88f92ec4d2a852010082263a70026844146d1539
Keith Zmudzinski <kzmudz79@gmail.com>
2020-09-03 18:42:31 -0700

    Updated framework

--- ehrp-api-master/WSD_test_harness.py
+++
@@ -1,8 +0,0 @@
-from WordSenseDisambiguation import WSD
-
-def get_data():
-    return test, train
-
-def evaluate(model, data):
-    # generate bunha info and scores
-    return
--- ehrp-api-master/WordSenseDisambiguation.py
+++ ehrp-api-master/WordSenseDisambiguation.py
@@ -1,15 +1,18 @@
 ''' Used for the determination of homonym meanings, given context '''
 
 class WSD:
-    def __init__(self):
+    def __init__(self, location=None):
+        self.location = location
         self.model = self.load_model()
 
     def get_meaning(self, instances, term, context):
         # placeholder
         return instances[0]
 
-    def load_model(self):
-        pass
+    def load_model(self, path=self.location):
+        if self.location:
+            return loaded_model
+        return None
 
-    def save_model(self):
+    def save_model(self, path=self.location):
         pass
